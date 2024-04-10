d16cc93f54438c15fccbf7821fe7f3eca3d47100
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-04-10 19:52:55 -0400

    update style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import data from "./data.json";
 import SchoolList from "./Components/SchoolList";
-
+import "./App.css";
 function App() {
   // const data = data
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Schools and Band Accessories</h1>
+        <h1 className="heading">School Landing Pages</h1>
         <SchoolList data={data} />
       </header>
     </div>
