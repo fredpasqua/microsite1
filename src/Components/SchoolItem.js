b39c1f7564a8454d37709032fcd11ba37779af98
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-04-10 19:35:43 -0400

    udpates

--- src/Components/SchoolItem.js
+++ src/Components/SchoolItem.js
@@ -1,18 +1,28 @@
 import React from "react";
-
+import "./SchoolItem.css";
 const SchoolItem = ({ school }) => {
   return (
-    <div>
-      <h3>{school.School}</h3>
-      <h4>{school.SubHeading}</h4>
-      <p>
-        {school.City}, {school.State}
-      </p>
-      <p>
-        <a href={school.URL} target="_blank" rel="noopener noreferrer">
-          View Page
-        </a>
-      </p>
+    <div className="schoolBanner">
+      <div className="schoolNameAddress">
+        <h3>{school.School}</h3>
+
+        <p>
+          {school.City}, {school.State}
+        </p>
+      </div>
+      <div className="right-side">
+        <h4 className="subheading">{school.SubHeading}</h4>
+        <p>
+          <a
+            href={school.URL}
+            className="link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            View Page
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
