cdbb0a7651b5c5e2b02d7badfa1e003f05fa6e3d
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-04-10 20:32:35 -0400

    add search function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
-import data from "./data.json";
+import { React, useState } from "react";
+
+import TextField from "@mui/material/TextField";
 import SchoolList from "./Components/SchoolList";
 import "./App.css";
 function App() {
-  // const data = data
-
+  const [inputText, setInputText] = useState("");
+  let inputHandler = (e) => {
+    //convert input text to lower case
+    var lowerCase = e.target.value.toLowerCase();
+    setInputText(lowerCase);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="heading">School Landing Pages</h1>
-        <SchoolList data={data} />
+        <div className="search">
+          <TextField
+            id="outlined-basic"
+            onChange={inputHandler}
+            variant="outlined"
+            fullWidth
+            label="Search"
+          />
+        </div>
+        <SchoolList input={inputText} />
       </header>
     </div>
   );
--- src/Components/SchoolList.js
+++ src/Components/SchoolList.js
@@ -1,10 +1,24 @@
 import React from "react";
 import SchoolItem from "./SchoolItem";
-
-const SchoolList = ({ data }) => {
+import data from "../data.json";
+const SchoolList = (props) => {
+  //create a new array by filtering the original array
+  const filteredData = data.filter((el) => {
+    //if no input the return the original
+    if (props.input === "") {
+      return el;
+    }
+    //return the item which contains the user input
+    else {
+      return (
+        el.Name.toLowerCase().includes(props.input) ||
+        el.City.toLowerCase().includes(props.input)
+      );
+    }
+  });
   return (
     <div>
-      {data.map((school, index) => (
+      {filteredData.map((school, index) => (
         <SchoolItem key={index} school={school} />
       ))}
     </div>
