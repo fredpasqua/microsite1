5d9c4971edb4256e886f8f264871a191301592ed
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-04-13 08:55:58 -0400

    updates

--- src/Components/HeaderElement.js
+++ src/Components/HeaderElement.js
@@ -4,35 +4,36 @@ import Button from "@mui/material/Button";
 function HeaderElement() {
   return (
     <div className="headerContainer">
-      <div className="sectionContainer">
-        <section className="section1">1</section>
-        <section className="categoryLinks">
-          <Button variant="contained" href={"#"} className="button">
-            Elementary Band
-          </Button>{" "}
-          <Button variant="contained" href={"#"} className="button">
-            Middle School Band
-          </Button>{" "}
-          <Button variant="contained" href={"#"} className="button">
-            High School Band
-          </Button>{" "}
-          <Button variant="contained" href={"#"} className="button">
-            Elementary Strings
-          </Button>{" "}
-          <Button variant="contained" href={"#"} className="button">
-            Middle School Strings
-          </Button>{" "}
-          <Button variant="contained" href={"#"} className="button">
-            High School Strings
-          </Button>{" "}
-          <Button variant="contained" href={"#"} className="button">
-            Jazz Band
-          </Button>
-          <Button variant="contained" href={"#"} className="button">
-            Marching Band
-          </Button>
-        </section>
-      </div>
+      <section className="section1">1</section>
+      <section className="categoryLinks">
+        <div>
+          <h2 className="linkHeading">Shop By Category</h2>
+        </div>
+        <Button variant="contained" href={"#"} className="button">
+          Elementary Band
+        </Button>{" "}
+        <Button variant="contained" href={"#"} className="button">
+          Middle School Band
+        </Button>{" "}
+        <Button variant="contained" href={"#"} className="button">
+          High School Band
+        </Button>{" "}
+        <Button variant="contained" href={"#"} className="button">
+          Elementary Strings
+        </Button>{" "}
+        <Button variant="contained" href={"#"} className="button">
+          Middle School Strings
+        </Button>{" "}
+        <Button variant="contained" href={"#"} className="button">
+          High School Strings
+        </Button>{" "}
+        <Button variant="contained" href={"#"} className="button">
+          Jazz Band
+        </Button>
+        <Button variant="contained" href={"#"} className="button">
+          Marching Band
+        </Button>
+      </section>
     </div>
   );
 }
