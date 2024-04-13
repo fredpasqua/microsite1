69ad00d10605d4bfeab4c8c6b84fb71e3e6b6352
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-04-13 08:46:05 -0400

    updates

--- src/Components/HeaderElement.js
+++ src/Components/HeaderElement.js
@@ -1,10 +1,38 @@
 import React from "react";
 import "./HeaderElement.css";
+import Button from "@mui/material/Button";
 function HeaderElement() {
   return (
     <div className="headerContainer">
-      <section className="section1">1</section>
-      <section className="section2">2</section>
+      <div className="sectionContainer">
+        <section className="section1">1</section>
+        <section className="categoryLinks">
+          <Button variant="contained" href={"#"} className="button">
+            Elementary Band
+          </Button>{" "}
+          <Button variant="contained" href={"#"} className="button">
+            Middle School Band
+          </Button>{" "}
+          <Button variant="contained" href={"#"} className="button">
+            High School Band
+          </Button>{" "}
+          <Button variant="contained" href={"#"} className="button">
+            Elementary Strings
+          </Button>{" "}
+          <Button variant="contained" href={"#"} className="button">
+            Middle School Strings
+          </Button>{" "}
+          <Button variant="contained" href={"#"} className="button">
+            High School Strings
+          </Button>{" "}
+          <Button variant="contained" href={"#"} className="button">
+            Jazz Band
+          </Button>
+          <Button variant="contained" href={"#"} className="button">
+            Marching Band
+          </Button>
+        </section>
+      </div>
     </div>
   );
 }
