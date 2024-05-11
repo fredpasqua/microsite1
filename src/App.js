e3c03d90b37221447dd3c473a7fc3d1eae32a1a2
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-05-10 22:15:10 -0400

    added switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { React, useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SchoolPage from './SchoolPage'; // Assuming SchoolPage component is in a separate file
 import TextField from "@mui/material/TextField";
 import SchoolList from "./Components/SchoolList";
 import "./App.css";
@@ -11,24 +13,28 @@ function App() {
     setInputText(lowerCase);
   };
   return (
-    <div className="App">
-      <HeaderElement></HeaderElement>
-      <div className="searchBox">
-        <h2 className="searchHeader">Find Accessories by School</h2>
-        <div className="search">
-          <TextField
-            id="outlined-basic"
-            onChange={inputHandler}
-            variant="outlined"
-            fullWidth
-            label="search"
-          />
-        </div>
-        <div className="searchContainer">
-          <SchoolList input={inputText} />
-        </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <HeaderElement></HeaderElement>
+          <div className="searchBox">
+            <h2 className="searchHeader">Find Accessories by School</h2>
+            <div className="search">
+              <TextField
+                id="outlined-basic"
+                onChange={inputHandler}
+                variant="outlined"
+                fullWidth
+                label="search"
+              />
+            </div>
+            <div className="searchContainer">
+              <SchoolList input={inputText} />
+            </div>
+          </div>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
